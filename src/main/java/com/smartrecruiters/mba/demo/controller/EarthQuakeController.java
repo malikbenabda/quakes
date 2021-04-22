951defdee188d3e7306e2a4a371f98219a1db36e
fghedira <fakhreddine.ghedira@gmail.com>
2021-04-22 23:57:35 +0100

    fixed thymleaf

--- src/main/java/com/smartrecruiters/mba/demo/controller/EarthQuakeController.java
+++ src/main/java/com/smartrecruiters/mba/demo/controller/EarthQuakeController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -16,18 +17,23 @@ public class EarthQuakeController {
     @Autowired
     USGCService usgcService;
     @GetMapping("/quakes")
-    public String nearbyQuakeList(ModelAndView model, @RequestParam Double lat, @RequestParam Double lon){
+//    public String nearbyQuakeList(ModelAndView model, @RequestParam Double lat, @RequestParam Double lon){
+//
+//        //model.addAttribute("quakes", usgcService.getEarthquakesAround(lat,lon));
+//        List<String> listOfPlaces = usgcService.calculateNearestEarthquakes(lat,lon);
+//        model.addObject("quakes", listOfPlaces);
+//        return "quakes";
+//    }
+    @ResponseBody
+    public ModelAndView nearbyQuakeList(Model model1, ModelAndView model, @RequestParam Double lat, @RequestParam Double lon){
 
         //model.addAttribute("quakes", usgcService.getEarthquakesAround(lat,lon));
         List<String> listOfPlaces = usgcService.calculateNearestEarthquakes(lat,lon);
         model.addObject("quakes", listOfPlaces);
-        return "quakes";
-    }
-
-    @GetMapping("/")
-    public String welcome(Model model){
-
-        return "quakesddd";
+        model1.addAttribute("quakes", listOfPlaces);
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("quakes.html");
+        return modelAndView;
     }
     
 }
