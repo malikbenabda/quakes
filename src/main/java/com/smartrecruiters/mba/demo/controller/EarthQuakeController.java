e74ce67f428dc9a3b7fae083576889ebc3f12f10
fghedira <fakhreddine.ghedira@gmail.com>
2021-04-22 23:34:43 +0100

    fixed pathes

--- src/main/java/com/smartrecruiters/mba/demo/controller/EarthQuakeController.java
+++ src/main/java/com/smartrecruiters/mba/demo/controller/EarthQuakeController.java
@@ -2,32 +2,45 @@ package com.smartrecruiters.mba.demo.controller;
 
 import com.smartrecruiters.mba.demo.service.USGCService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class EarthQuakeController {
 
     @Autowired
     USGCService usgcService;
     @GetMapping("/quakes")
-    public String nearbyQuakeList(ModelAndView model, @RequestParam Double lat, @RequestParam Double lon){
+    @ResponseBody
+    public ModelAndView nearbyQuakeList(ModelAndView model, @RequestParam Double lat, @RequestParam Double lon){
 
         //model.addAttribute("quakes", usgcService.getEarthquakesAround(lat,lon));
         List<String> listOfPlaces = usgcService.calculateNearestEarthquakes(lat,lon);
         model.addObject("quakes", listOfPlaces);
-        return "quakes";
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("quakes.html");
+        return modelAndView;
     }
 
+//    @GetMapping("/")
+//    @ResponseBody
+//    public String welcome(){
+//
+//        return "index.html";
+//    }
     @GetMapping("/")
-    public String welcome(Model model){
-
-        return "quakesddd";
+    @ResponseBody
+    public ModelAndView welcome() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
     }
     
 }
